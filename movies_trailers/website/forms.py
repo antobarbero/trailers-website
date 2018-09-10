70a9962c347f86902bc250ca32317fd4b0f64ba2
anto <antobarbero94@gmail.com>
2018-09-10 11:43:50 -0300

    fix flake8 errors

--- movies_trailers/website/forms.py
+++ movies_trailers/website/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-	"""Form for movies searching."""
-	search_word = forms.CharField(max_length=64, min_length=1)
+    """Form for movies searching."""
+    search_word = forms.CharField(max_length=64, min_length=1)
--- movies_trailers/website/views.py
+++ movies_trailers/website/views.py
@@ -3,7 +3,6 @@ from .forms import SearchForm
 
 
 class SearchView(FormView):
-	"""Manage the searching view at index page."""
-	form_class = SearchForm
-	template_name = 'website/index.html'
-
+    """Manage the searching view at index page."""
+    form_class = SearchForm
+    template_name = 'website/index.html'
