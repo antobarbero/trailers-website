1a935fb06a2e16860ef2d5dbdf1b02de02f2235a
anto <antobarbero94@gmail.com>
2018-09-11 11:19:04 -0300

    first approach to api view

--- movies_trailers/trailers_API/views.py
+++ movies_trailers/trailers_API/views.py
@@ -1,41 +1,23 @@
-import json
-
-import requests
-from django.http import Http404
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
-class OMDb:
-    """Class to call Open Movie Database API for movies search."""
-    base_url = 'http://www.omdbapi.com/'
-    api_key = '28e7ad89'
-
-    @classmethod
-    def get_movies(cls, search_words):
-        """Returns a list of movies that matches with the search word."""
-        params = {
-            'apikey': cls.api_key,
-            's': search_words
-        }
-
-        response = requests.get(cls.base_url, params=params)
-
-        if response.ok:
-            dict_response = json.loads(response.text)
-            movies_result = dict_response.get('Search')
-        else:
-            raise Http404
-
-        return movies_result
+from .utils import omdb, youtube
 
 
-class MoviesTrailersAPIView(OMDb, APIView):
+class MoviesTrailersAPIView(APIView):
     """API View to retrieve the movies and its trailers."""
     renderer_classes = JSONRenderer,
 
     def get(self, request):
-        search_words = request.query_params.get('s')
-        response_dict = self.get_movies(search_words)
-        return Response(response_dict)
+
+        key_words = request.query_params.get('q')
+        movies = omdb.get_movies(key_words)
+
+        for movie in movies:
+            trailers = youtube.get_trailers(movie.get('Title'))
+            movie.update({'yt_trailers_ids': [
+                trailer['id']['videoId'] for trailer in trailers
+            ]})
+
+        return Response(movies)
