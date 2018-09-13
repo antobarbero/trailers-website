41c08211f71cd2c4eca36c60ff17027050710bb8
anto <antobarbero94@gmail.com>
2018-09-13 07:06:07 -0300

    rename form field

--- movies_trailers/website/forms.py
+++ movies_trailers/website/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class SearchForm(forms.Form):
     """Form for movies searching."""
-    search_word = forms.CharField(max_length=64, min_length=1)
+    key_words = forms.CharField(max_length=64, min_length=1)
