df1d1ee6ebea3469b105359b26cbad19fb2b57cb
anto <antobarbero94@gmail.com>
2018-09-13 16:40:48 -0300

    ajax html render

--- movies_trailers/website/static/website/js/ajax.js
+++ movies_trailers/website/static/website/js/ajax.js
@@ -2,15 +2,32 @@ $(document).ready(function(){
   $("#search-trailers").submit(function(e){
     e.preventDefault();
     var q = $("#id_key_words").val();
-
+    document.getElementById('result-container').innerHTML = '';
     $.ajax({
       dataType: 'json',
       url: $(this).attr('action') + '?q=' + q,
       type: $(this).attr('method'),
       data: {},
       success: function(json){
-        console.log(json)
+        json.forEach((movie) => {
+            document.getElementById('result-container').innerHTML += renderItem(movie.Title, movie.trailers);
+        });
       }
     });
   })
-})
+});
+
+function renderItem(title, trailers) {
+  var html = '<div class="row no-gutters"><div class="col-lg-12 showcase-text"><h2>' + title + '</h2></div>';
+
+  trailers.forEach ((trailer) => {
+    html += '<div class="col-lg-12 text-white showcase-img" align="center"><iframe width="600" height="400" src="https://www.youtube.com/embed/' + trailer + '"></iframe></div>';
+  });
+
+  if (trailers.length === 0) {
+    html += '<div class="col-lg-12 showcase-text" align="center">No trailer found.</div>';
+  }
+  html += '</div><hr>';
+
+  return html;
+}
