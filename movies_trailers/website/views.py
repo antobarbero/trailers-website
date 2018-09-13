e5a97e07184f5dca308215f2e06fa15c3ebd9f50
anto <antobarbero94@gmail.com>
2018-09-13 16:57:35 -0300

    docstring

--- movies_trailers/website/views.py
+++ movies_trailers/website/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import FormView
+
 from .forms import SearchForm
 
 
 class SearchView(FormView):
-    """Manage the searching view at index page."""
+    """Manage the search of movies."""
     form_class = SearchForm
     template_name = 'website/index.html'
