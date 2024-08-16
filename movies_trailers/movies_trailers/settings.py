390b01d0f95e4cd6581b5be5f0e6d2e83804a503
Antonella Barbero <antobarbero94@gmail.com>
2024-08-16 14:07:41 +0200

    Update settings.py

--- movies_trailers/movies_trailers/settings.py
+++ movies_trailers/movies_trailers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')2bh-epyyk6ujk1izmhl@2!$!!kislll8c8fv3z%*t&%=7lh%c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
